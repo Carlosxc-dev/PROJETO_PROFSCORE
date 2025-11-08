cfc112d355253a8edba4136d2b5f1c9b891af52a
Carlosxc-dev <chcruz64@gmail.com>
2025-11-08 14:31:14 -0300

    fix(app): ajustando arquivos

--- backend/src/core/enums/position.ts
+++
@@ -1,4 +0,0 @@
-export enum Position {
-	deitado = "deitado",
-	ereto = "ereto",
-}
--- backend/src/infrastructure/database/prisma/migrations/20251014162841_alter_image4_k/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "images_4k" ADD COLUMN     "user_id" TEXT;
-
--- AddForeignKey
-ALTER TABLE "images_4k" ADD CONSTRAINT "images_4k_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
