4c51f6878e12de3f104c955f3f11f4f6776b84ed
Thiago <thiagobg1811@gmail.com>
2025-11-12 14:56:46 -0300

    feat: prototipo teste login

---
+++ selenium-tests/login_test.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
+
+driver.get("http://localhost:4200/login")
+time.sleep(1) #Esperar 1 segundo para o carregamento visual
+
+# Input de email
+email_input = wait.until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[placeholder="Email do Usuário"]'))
+)
+email_input.send_keys("usuario@teste.com")
+time.sleep(2)
+
+# Input de senha (ajustar seletor conforme HTML real)
+password_input = wait.until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'app-input-group input[type="password"]'))
+)
+password_input.send_keys("senha123")
+time.sleep(2)
+
+login_button = wait.until(
+    EC.presence_of_element_located(
+        (By.XPATH, '//app-login//*[contains(text(), "Enviar")]')
+    )
+)
+driver.execute_script("arguments[0].click();", login_button)
+time.sleep(2)
